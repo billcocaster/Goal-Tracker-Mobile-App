810508d532ca021a8da43cfa7c0ee47d78269790
billcocaster <116905530+billcocaster@users.noreply.github.com>
2025-01-25 17:42:23 +0300

    Adding goals and displaying them in a list

--- GoalTrackerApp/App.js
+++ GoalTrackerApp/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[enteredGoalText, setEnteredGoalText] = useState('');
+  const[goals, setGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  };
+  function addGoalHandler() {
+    setGoals(currentGoals => [...goals, enteredGoalText]);
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your goal' onChangeText={goalInputHandler}/>
+        <Button title='Add goal' onPress={addGoalHandler}/>
+      </View>
+      <View>
+        {goals.map((goal) => <Text>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  appContainer: {
+    padding: 50,
+    backgroundColor: 'white',
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
